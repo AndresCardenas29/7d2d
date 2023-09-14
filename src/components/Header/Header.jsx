75fb221f4b10cd4f911cb99eafd8663b7f7c4f99
Andrés Cárdenas <mandrescardenash@gmail.com>
2023-09-13 19:42:32 -0500

    se cambio el boton de contraseña

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -27,7 +27,7 @@ export default function HeaderPage() {
 				</button>
 				<button
 					onClick={() => {
-						let text = "hola";
+						let text = "7d2d.nekdress.online:25700";
 						const textoLimpio = text.replace(/;/g, "");
 						const el = document.createElement("textarea");
 						el.value = textoLimpio;
@@ -36,11 +36,12 @@ export default function HeaderPage() {
 						document.execCommand("copy");
 						document.body.removeChild(el);
 						alert(
-							"Contraseña pegada en el portapapeles. La contraseña es: hola"
+							"IP del servidor pegada en el portapapeles. La IP es: 7d2d.nekdress.online:25700"
 						);
 					}}
 				>
-					<svg
+					IP SERVER
+					{/* <svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
@@ -53,7 +54,7 @@ export default function HeaderPage() {
 							strokeLinejoin="round"
 							d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
 						/>
-					</svg>
+					</svg> */}
 				</button>
 			</div>
 			<div className="basis-3/4">
